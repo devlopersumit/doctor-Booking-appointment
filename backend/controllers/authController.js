8e5d95583f836ce3a082ef5c6dbdcefb569958d9
devlopersumit <sumitsj2002@gmail.com>
2026-01-04 17:04:02 +0530

    feat: enhance user registration with input validation and error handling

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,40 @@
+const inputValidator = require("../helpers/validation");
+const bcrypt =  require('bcryptjs');
+const User = require("../models/User");
 
 
 //User Registration
 const signup = async (req, res) => {
-    const inputValues = req.body;
-    const { name, email, password, role } = inputValues; 
+    const {name, email, password, role} = req.body;
+
+   try {
+     inputValidator(name, email, password, role);
+
+    const user = await User.findOne(email);
+    if(user) {
+      return res.status(400).json({success:false, message:'User already exist'});
+    }
+
+    const newUser = await User.create({
+        name,
+         email, 
+        password,
+         role
+    });
+
+    return res.status(201).json({
+        success:true,
+        message:'User Created Successfully',
+        user: {
+            name, email
+        }
+    })
+
+   } catch (error) {
+    return res.status(500).json({
+        success:false,
+        message:'Error occured in user creation: ' + error.message
+    })
+   }
+
 }
